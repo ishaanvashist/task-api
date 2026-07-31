20cccf33c84a5a35368a254a04a26a25eb865285
Ishaan Vashist <vashistishaan@gmail.com>
2026-07-31 18:12:55 +0530

    Add Testcontainers to application context test

--- src/test/java/com/ishu/task_api/TaskApiApplicationTests.java
+++ src/test/java/com/ishu/task_api/TaskApiApplicationTests.java
@@ -2,9 +2,29 @@ package com.ishu.task_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TaskApiApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:16")
+					.withDatabaseName("taskdb")
+					.withUsername("taskuser")
+					.withPassword("taskpass");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
